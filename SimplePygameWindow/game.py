f9b1e1a054969e435ae8c84dcc2bb72a2e8d8eb2
HuyDD305 <bribribridu@gmail.com>
2025-01-03 21:50:45 +0700

    entities

--- SimplePygameWindow/game.py
+++ SimplePygameWindow/game.py
@@ -12,23 +12,28 @@ class Game:
         self.img_pos = [160, 260]
         self.movement = [False, False]
         self.up_down = [False, False]
-        self.collision_area = pygame.Rect(50, 50, 300, 50) #tao mot cai hinh chu nhat phuc vu cho collision
+        self.collision_area = pygame.Rect(50, 50, 300, 50)  # tao mot cai hinh chu nhat phuc vu cho collision
 
     def run(self):
         while True:
             self.screen.fill((14, 219, 248))
-            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            # self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
             # self.img_pos[0] += (self.up_down[1] - self.up_down[0]) * 5
-            self.screen.blit(self.img, self.img_pos)  # day la toa do(x = 100, y = 200)(top left la (0, 0)),blit dung de paste cai surface nay len surface khac
+            # self.screen.blit(self.img, self.img_pos)  # day la toa do(x = 100, y = 200)(top left la (0, 0)),blit dung de paste cai surface nay len surface khac
 
             # tao mot cai rect nhung cai rect nay co the hieu nhu la logic de phat hien collision'
             # cai rect nay se they doi vi tri theo cai img
             img_r = pygame.Rect(self.img_pos[0], self.img_pos[1], self.img.get_width(), self.img.get_height())
             if img_r.colliderect(self.collision_area):
-                #ve truc tiep len cai screen surface chu ko phai la tao them mot cai surface rieng biet nhu blit, co nghia la ko di chuyen duoc
+                # ve truc tiep len cai screen surface chu ko phai la tao them mot cai surface rieng biet nhu blit, co nghia la ko di chuyen duoc
                 pygame.draw.rect(self.screen, (0, 100, 255), self.collision_area)
             else:
                 pygame.draw.rect(self.screen, (0, 50, 155), self.collision_area)
+
+            self.img_pos[1] += (self.movement[1] - self.movement[0]) * 5
+            self.img_pos[0] += (self.up_down[1] - self.up_down[0]) * 5
+            self.screen.blit(self.img,
+                             self.img_pos)  # day la toa do(x = 100, y = 200)(top left la (0, 0)),blit dung de paste cai surface nay len surface khac
 
             for event in pygame.event.get():  # Cai nay se tra lo mot cai list gom cac event
                 if event.type == pygame.QUIT:  # Moi cai event thi se co mot cai type, day la code de dung dau X thoat chuong trinh
@@ -40,20 +45,20 @@ class Game:
                         self.movement[0] = True
                     if event.key == pygame.K_DOWN:
                         self.movement[1] = True
-                    # if event.key == pygame.K_LEFT:
-                    #     self.up_down[0] = True
-                    # if event.key == pygame.K_RIGHT:
-                    #     self.up_down[1] = True
+                    if event.key == pygame.K_LEFT:
+                        self.up_down[0] = True
+                    if event.key == pygame.K_RIGHT:
+                        self.up_down[1] = True
 
                 if event.type == pygame.KEYUP:  # day la de biet la co key dang duoc lift
                     if event.key == pygame.K_UP:  # day la de check xem dang nhan key gi
                         self.movement[0] = False
                     if event.key == pygame.K_DOWN:
                         self.movement[1] = False
-                    # if event.key == pygame.K_LEFT:
-                    #     self.up_down[0] = False
-                    # if event.key == pygame.K_RIGHT:
-                    #     self.up_down[1] = False
+                    if event.key == pygame.K_LEFT:
+                        self.up_down[0] = False
+                    if event.key == pygame.K_RIGHT:
+                        self.up_down[1] = False
 
             pygame.display.update()
             # den buoc nay se hien len mot cai window nhung vi neu mot cai app ma ko co input
